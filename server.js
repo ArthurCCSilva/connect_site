653a15d810e41e8e6fcf57504ce5ceca70ba4bb2
ArthurCCSilva <arthurccs2013@gmail.com>
2025-04-24 20:47:44 -0300

    corrigindo erro de servidor
    corrigindo erro de conexão com servidor

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 import express from 'express'//ir na package.json e criar "type":'module',
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
+const prisma = new PrismaClient();
 const app = express() //Aqui crio a variavel app para receber a função express.A função ira receber a biblioteca do express
 app.use(express.json())//Para usar json
 
-let users =[]
 
-app.post('/users', (req, res) =>{//metodo para add usuário
+const users =[]
+
+
+app.post('/users', async(req, res) =>{//metodo para add usuário
+    //usuarios por conta do model em schema.prisma
+    await prisma.usuarios.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
     //console.log(req.body)// modo de add usuario com o json
-    users.push(req.body)//colocando o usuario na constante users
-    res.send('Usuário adicionado!')
+    //users.push(req.body)//colocando o usuario na constante users
+    res.status(201).json(req.body)//status 201 deu ok e criou o usuário
 })
 
 app.get('/users', (req, res) =>{//(LISTAR)Rota do metodo GET() no endereço -> users
 
-    res.json(users)//Aqui vem a resposta.
+    res.status(200).json(users)//Deu ok status 200
 }) 
 
 app.listen(3000)//porta do localhost
