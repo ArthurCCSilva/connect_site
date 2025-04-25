4a4adbb12fc09bc9e4c6198e949c00dfba534cc3
ArthurCCSilva <arthurccs2013@gmail.com>
2025-04-24 23:18:15 -0300

    update
    alterar usuário

--- server.js
+++ server.js
@@ -25,9 +25,32 @@ app.post('/users', async(req, res) =>{//metodo para add usuário
     res.status(201).json(req.body)//status 201 deu ok e criou o usuário
 })
 
-app.get('/users', (req, res) =>{//(LISTAR)Rota do metodo GET() no endereço -> users
+app.get('/users', async(req, res) =>{//(LISTAR)Rota do metodo GET() no endereço -> users
+    const usuarios = await prisma.usuarios.findMany()
+    res.status(200).json(usuarios)//Deu ok status 200
+})
 
-    res.status(200).json(users)//Deu ok status 200
-}) 
+app.put('/users/:id', async(req, res) =>{//metodo para add mudar várias informações do usuário
+        
+    await prisma.usuarios.update({
+        where:{
+            id:req.params.id //param é quan quer especificar 1 usuario específico
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    
+    res.status(201).json(req.body)//status 201 deu ok e criou o usuário
+})
 
 app.listen(3000)//porta do localhost
+
+//GET - Lista usuario
+//POST - Cria usuario
+//PUT - Edita vários
+//PATCH - Edita UM
+//DELETE - Deletar usuário
